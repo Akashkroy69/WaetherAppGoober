1c2b2d6087b1f78bd1d80047f94e381185da4720
Akashkroy69 <Akashkroy@hotmail.com>
2021-07-12 18:57:46 +0530

    -created a proper layout file Activity_main.xml which has an ImageView for location icon, A textView to instruct to enter Zip code, an EditText to take User Input, and a button to act to show required output. -Wrote logic in MainActivity.kt to Toast zipcode. -In MainActivity.kt we found ids for "How is weather" button and Id for EditText field.
    App is working as expected.

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ I am creating this app as part of my learning process. I have created some apps 
                  -Using Repositories
                  -LiveData
                  -MVVM App Architecture
+                 -Using XML still for layout files (Probably will refactor into Android Jetpack,
+                 Now don't want to increase the load)
+
+# Creating a layout file using XML
+               -created a proper layout file Activity_main.xml which has an ImageView for location icon,
+               A textView to instruct to enter Zip code, an EditText to take User Input, and a button to
+               act to show required output.
+
+
--- app/src/main/java/com/aks/waetherappgoober/MainActivity.kt
+++ app/src/main/java/com/aks/waetherappgoober/MainActivity.kt
@@ -2,10 +2,21 @@ package com.aks.waetherappgoober
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val enterZipcodeField: EditText = findViewById(R.id.zipCodeId)
+        val howIsWeatherButtonId: Button = findViewById(R.id.howIsWeatherButtonId)
+
+        howIsWeatherButtonId.setOnClickListener {
+            val zipcode = enterZipcodeField.text.toString()
+            if (zipcode.length == 6) Toast.makeText(this, zipcode, Toast.LENGTH_SHORT).show()
+            else Toast.makeText(this, "Please enter a valid Zip Code !!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
