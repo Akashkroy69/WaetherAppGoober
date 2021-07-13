1158c1d3eb11e9842c7806bc587554754473ed28
Akashkroy69 <Akashkroy@hotmail.com>
2021-07-13 14:57:24 +0530

    -created a proper layout file Activity_main.xml which has an ImageView for location icon, A textView to instruct to enter Zip code, an EditText to take User Input, and a button to act to show required output. -Wrote logic in MainActivity.kt to Toast zipcode. -In MainActivity.kt we found ids for "How is weather" button and Id for EditText field.
    App is working as expected.
    Some minor changes.

--- app/src/main/java/com/aks/waetherappgoober/MainActivity.kt
+++ app/src/main/java/com/aks/waetherappgoober/MainActivity.kt
@@ -7,16 +7,40 @@ import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    //region set up methods
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val enterZipcodeField: EditText = findViewById(R.id.zipCodeId)
         val howIsWeatherButtonId: Button = findViewById(R.id.howIsWeatherButtonId)
-
+        //Setting up On-Click Listener.
         howIsWeatherButtonId.setOnClickListener {
             val zipcode = enterZipcodeField.text.toString()
             if (zipcode.length == 6) Toast.makeText(this, zipcode, Toast.LENGTH_SHORT).show()
-            else Toast.makeText(this, "Please enter a valid Zip Code !!", Toast.LENGTH_SHORT).show()
+            else Toast.makeText(this, R.string.zipcode_error_message, Toast.LENGTH_SHORT).show()
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    //endregion set up methods
+    // region tear up methods
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+    //endregion tear up methods
 }
